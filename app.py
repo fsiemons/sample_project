6f32718cd664e408ff9f0ef9055285bf86a35b47
Felix Siemons <siemons.felix@gmx.de>
2024-06-05 13:44:16 +0200

    changed application to add numbers

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from argparse import ArgumentParser
 
 
-parser = ArgumentParser(description='Useless program that makes you answer your own questions')
-parser.add_argument('question', help='a string that will be asked')
+parser = ArgumentParser(description='Minimal program that adds 2 numbers')
+parser.add_argument('first_number', help='First number to be added together with the second')
+parser.add_argument('second_number', help='Second number to be added together with the first')
 args = parser.parse_args()
 
-answer = input(f"Your question today is:\n{args.question}?\n")
 
-print(f"You answered with:\n{answer}")
+def add(a, b):
+
+    return float(a) + float(b)
+
+
+print(add(args.first_number, args.second_number))
